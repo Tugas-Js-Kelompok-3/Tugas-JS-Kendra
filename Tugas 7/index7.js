765fd05d08e5cd9132b3f8e3717d7d9af6aeb72f
fernfd <kendrawow123@gmail.com>
2022-01-31 14:57:16 +0700

    javascript

---
+++ Tugas 7/index7.js
@@ -0,0 +1,22 @@
+// Soal 1
+function trafficLight(lampu) {
+    switch(lampu) {
+        case "red" :
+            return 'Stop';
+        break;
+        case "yellow" :
+            return 'Be Careful';
+        break;
+        case "green" :
+            return 'Go!';
+        break;
+    }
+}
+console.log(trafficLight("green"));
+
+// Soal 2
+
+const persegi = (sisi) => {
+    return sisi * sisi**2
+}
+console.log(persegi(11))
